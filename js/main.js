c2dbb9846fb9830b9f94646b20e041ba85632299
Alphy Jose <alphyjose012@gmail.com>
2024-09-11 19:00:03 +0530

    Re-designed login Page completely and removed message.html and msgStyle.css

--- js/main.js
+++ js/main.js
@@ -1,28 +1,11 @@
-// function to go to sign up & sign in from login page with sound
-function loadPage(nextPageId) {
-    const currentPage = document.querySelector('.active');
-    const nextPage = document.getElementById(nextPageId);
-    const transitionSound = document.getElementById('transition-sound');
+const signInButton = document.getElementById('signIn');
+const signUpButton = document.getElementById('signUp');
+const container = document.getElementById('container');
 
-    // Play the sound
-    transitionSound.play();
+signUpButton.addEventListener('click', () => {
+  container.classList.add('right-panel-active');
+});
 
-    // Add class to slide the current page out
-    currentPage.classList.add('slide-out');
-    
-    // After the slide-out animation, switch pages
-    setTimeout(function() {
-        // Remove the active class and transition classes from the current page
-        currentPage.classList.remove('active');
-        currentPage.classList.remove('slide-out');
-
-        // Add classes to make the new page slide in
-        nextPage.classList.add('active');
-        nextPage.classList.add('slide-in');
-
-        // Clean up after the animation
-        setTimeout(function() {
-            nextPage.classList.remove('slide-in');
-        }, 500); // Match this duration with your CSS transition time
-    }, 500); // Match this duration with your CSS transition time
-}
+signInButton.addEventListener('click', () => {
+  container.classList.remove('right-panel-active');
+});
